0e37b75a5974aa4e080e8aa7d7961aadae5bf3aa
Alice <alice@Alice-2.local>
2025-07-27 09:59:40 +0800

    trees

--- Trees/treeV2.cpp
+++ Trees/treeV2.cpp
@@ -3,7 +3,21 @@
 int main()
 {
 
-    Tree *myTree;
-    myTree->createTree();
+    Tree myTree;
+    myTree.createTree();
+
+    // 前序遍历
+    cout << " 前序遍历 : " << endl;
+    myTree.Preorder(myTree.root);
+    cout << endl;
+
+    cout << " 后序遍历 : " << endl;
+    myTree.Postorder(myTree.root);
+    cout << endl;
+
+    cout << " 中序遍历 : " << endl;
+    myTree.Inorder(myTree.root);
+    cout << endl;
+
     return 0;
 }
--- Trees/treeV3.h
+++ Trees/treeV3.h
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <queue>
+#include "../Queue/QueueClass.h"
 using namespace std;
 
 class Tree
