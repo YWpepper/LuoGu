b818f5a411148c102179073741d1ae077513c870
Alice <alice@Alice-2.local>
2025-07-31 16:19:22 +0800

    sort0731

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
-    int arr[] = {8, 5, 7, 3, 2};
+    int arr[] = {5, 8, 7, 3, 2};
     int len = sizeof(arr) / sizeof(arr[0]);
     display(arr, len);
-    bubbleSort(arr, len);
+    // bubbleSort(arr, len);
+    // bubbleSort_re(arr, len);
+    insertSort(arr, len);
     display(arr, len);
     return 0;
 }
--- Sort/Sort.h
+++ Sort/Sort.h
@@ -22,13 +22,52 @@ void display(int arr[], int len)
 
 void bubbleSort(int arr[], int len)
 {
-    for (int i = 0; i < len; i++)
+    int cnt = 0;
+    for (int i = 0; i < len - 1; i++)
     {
-        for (int j = 0; j < len - i - 1; j++)
+        for (int j = 0; j < len - 1 - i; j++)
         {
+            cnt++;
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    cout << "Total caculate is: " << cnt << endl;
+}
+
+void bubbleSort_re(int arr[], int len)
+{
+    int cnt = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < len - 1 - i; j++)
+        {
+            cnt++;
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                flag = 1; // 只要执行了一次交换，就说明这个列表还没有排好序列；
+            }
+        }
+        // 如果执行完一轮之后，flag依然等于0，则可以证明已经排序成功，可以返回结果；
+        if (flag == 0)
+            break;
+    }
+    cout << "Total caculate is: " << cnt << endl;
+}
+
+void insertSort(int arr[], int len)
+{
+    for (int i = 1; i < len; i++)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            if (arr[j - 1] > arr[j])
+            {
+                swap(arr[j - 1], arr[j]);
             }
         }
     }
