8db27d2b1388e995ae3e8137f01845046d32fefa
Alice <alice@Alice-2.local>
2025-08-02 22:43:21 +0800

    sort

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -2,12 +2,16 @@
 
 int main()
 {
-    int arr[] = {5, 8, 7, 3, 2};
+    // int arr[] = {5, 8, 7, 3, 2};
+    // int arr[] = {2, 3, 5, 7, 8};
+    int arr[] = {8, 9, 1, 7, 2, 3, 5, 4, 6};
     int len = sizeof(arr) / sizeof(arr[0]);
     display(arr, len);
     // bubbleSort(arr, len);
     // bubbleSort_re(arr, len);
     insertSort(arr, len);
+    // selectSort(arr, len);
+    // shellSort(arr, len);
     display(arr, len);
     return 0;
 }
--- Sort/Sort.h
+++ Sort/Sort.h
@@ -61,14 +61,69 @@ void bubbleSort_re(int arr[], int len)
 
 void insertSort(int arr[], int len)
 {
+    int cnt = 0;
     for (int i = 1; i < len; i++)
     {
+        bool flag = true;
         for (int j = i; j >= 0; j--)
         {
             if (arr[j - 1] > arr[j])
             {
                 swap(arr[j - 1], arr[j]);
+                flag = false;
+                cnt++;
+            }
+            // 只要跟前一个数值比较的时候满足排序要求，则就不需要再跟前面的前面交换了
+            if (flag)
+                break;
+        }
+    }
+    cout << "Total caculate is: " << cnt << endl;
+}
+
+void selectSort(int arr[], int len)
+{
+    int key_idx = 0;
+    for (int i = 0; i < len; i++)
+    {
+        // 二重循环的目的只是为了找到最小数值的索引，然后插入到第 i 个位置
+        for (int j = i; j < len; j++)
+        {
+            if (arr[key_idx] > arr[j])
+            {
+                key_idx = j;
+            }
+        }
+        // 遍历完一轮之后只执行一次的交换操作
+        swap(arr[key_idx], arr[i]);
+    }
+}
+
+void shellSort(int arr[], int len)
+{
+    int slide = 0;
+    int shellCNT = 0;
+    // slide 用来控制间隔
+    for (slide = len / 2; slide > 1; slide = slide / 2)
+    {
+        cout << "slide is " << slide << endl;
+        // 用来按照[slide=2, idx=0], [slide=2, idx=1],[slide=2, idx=2]···
+        for (int idx = 0; idx + slide < len; idx++)
+        {
+            for (int j = idx; j + slide < len; j += slide)
+            {
+                if (arr[idx] > arr[idx + slide])
+                {
+                    shellCNT++;
+                    swap(arr[idx], arr[idx + slide]);
+                    cout << "swap " << arr[idx] << " " << arr[idx + slide] << endl;
+                }
             }
         }
     }
+    cout << "shellCNT is " << shellCNT << endl;
+    cout << "before slide is 1: " << endl;
+    display(arr, len);
+    cout << "when slide is 1: " << endl;
+    insertSort(arr, len);
 }
